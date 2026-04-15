718b7e7ea04bdd0658a56cc7ad50201bf45816d6
AyushThakali <thakaliaayush@gmail.com>
2026-04-15 14:40:45 +0545

    feat: redux store configured

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,3 +1,14 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
 export const api = createApi({
-    
-})
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:5000",
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem("token");
+      if (token) headers.set("Authorization", `Bearer ${token}`);
+      return headers;
+    },
+  }),
+  endpoints: () => ({}),
+});
